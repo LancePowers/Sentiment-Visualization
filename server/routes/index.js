367c87569986dbbbb87a9f684896dea1d251bfe2
Lance <Lance@thelpstory.com>
2015-11-04 15:36:13 -0700

    Heroku deployment

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var cheerio = require('cheerio');
 var config = require('../_config.js')
 
 console.log(config.ALCHEMY_KEY);
